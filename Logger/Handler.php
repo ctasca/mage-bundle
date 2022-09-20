619268c43fc527eddb1bdc009aaf6001de724541
Carlo Tasca <c.tasca.1971@gmail.com>
2022-09-20 12:48:54 +0200

    Add Module Logger

---
+++ Logger/Handler.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Ctasca\MageBundle\Logger;
+
+use Monolog\Logger;
+use Magento\Framework\Logger\Handler\Base;
+
+class Handler extends Base
+{
+    /**
+     * Logging level
+     * @var int
+     */
+    protected $loggerType = Logger::INFO;
+
+    /**
+     * File name
+     * @var string
+     */
+    protected $fileName = '/var/log/mage-bundle.log';
+}
---
+++ Logger/Logger.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace Ctasca\MageBundle\Logger;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Logger extends \Monolog\Logger
+{
+    /**
+     * Path to system config
+     */
+    const LOGGING_ENABLED_CONFIG_PATH = 'ctasca_magebundle/settings/logging_enabled';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private ScopeConfigInterface $scopeConfig;
+
+    /**
+     * @param string $name
+     * @param ScopeConfigInterface $scopeConfig
+     * @param array $handlers
+     * @param array $processors
+     */
+    public function __construct(
+        string $name,
+        ScopeConfigInterface $scopeConfig,
+        array $handlers = array(),
+        array $processors = array()
+    ) {
+        parent::__construct($name, $handlers, $processors);
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function info($message, array $context = array()): bool
+    {
+        if ($this->isLoggingEnabled()) {
+            return parent::info($message, $context);
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function error($message, array $context = array()): bool
+    {
+        if ($this->isLoggingEnabled()) {
+            return parent::error($message, $context);
+        }
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isLoggingEnabled(): bool
+    {
+        return (bool) $this->scopeConfig->getValue(self::LOGGING_ENABLED_CONFIG_PATH);
+    }
+}
