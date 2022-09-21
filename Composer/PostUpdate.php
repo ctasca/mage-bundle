e415083d99923ac0fdcc181eae991ce2666dbcc0
Carlo Tasca <c.tasca.1971@gmail.com>
2022-09-21 15:12:02 +0200

    Remove Framework files

--- Composer/PostUpdate.php
+++ Composer/PostUpdate.php
@@ -6,23 +6,21 @@ namespace Ctasca\MageBundle\Composer;
 use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 use Composer\Util\Filesystem as ComposerFilesystem;
-use Ctasca\MageBundle\Framework\Filesystem as MagentoFilesystem;
-use Ctasca\MageBundle\Framework\File;
-use Ctasca\MageBundle\Framework\DirectoryList;
+use Magento\Framework\Filesystem as MagentoFilesystem;
+use Magento\Framework\Filesystem\Io\File;
+use Magento\Framework\App\Filesystem\DirectoryList;
 
 class PostUpdate
 {
     /**
-     * Post update scripts execute after install and update
-     *
+     * Post update scripts
      * @param Event $event
      * @return void
      */
     public static function copyTemplates(Event $event): void
     {
         $magentoFile = new File();
-        $skeletonDir = $event->getComposer()->getConfig()->get('ctasca/mage-bundle/Bundle/Skeleton');
-        var_dump($skeletonDir);
+        $skeletonDir = $event->getComposer()->getConfig()->get('mage-bundle/Bundle/Skeleton');
         $magentoFilesystem = new MagentoFilesystem();
         $composerFilesystem = new ComposerFilesystem();
         $mediaDirectory = $magentoFilesystem->getDirectoryRead(DirectoryList::MEDIA)
