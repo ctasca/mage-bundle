3155a2b680d6a13ba9083ca1a9325e84c0f13a74
Carlo Tasca <c.tasca.1971@gmail.com>
2023-01-16 14:22:31 +0100

    Fix ReflectionClass not found

--- Bundle/Skeleton/model-set/model/model-implementing-data-interface.tpl.php
+++ Bundle/Skeleton/model-set/model/model-implementing-data-interface.tpl.php
@@ -5,14 +5,14 @@ namespace {{model_namespace}};
 
 use Magento\Framework\Model\AbstractModel;
 use {{use_resource_model}} as {{class_name}}ResourceModel;
-use {{module_namespace}}\Api\Data\{{data_interface}}Interface;
+use {{module_namespace}}\Api\Data\{{data_interface}};
 
-class {{class_name}} extends AbstractModel implements {{data_interface}}Interface
+class {{class_name}} extends AbstractModel implements {{data_interface}}
 {
     protected function _construct(): void
     {
         $this->_init({{class_name}}ResourceModel::class);
     }
 
-    // TODO Implement the Data Interface
+    // TODO Implement the Interface
 }
