ecf7a925f9f2c27103b952d8946a8ca98d723e39
Carlo Tasca <c.tasca.1971@gmail.com>
2023-01-24 15:58:10 +0100

    Add widget block template

---
+++ Bundle/Skeleton/block/widget-block.tpl.php
@@ -0,0 +1,19 @@
+{{php}}
+declare(strict_types=1);
+
+namespace {{namespace}};
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class {{class_name}} extends Template implements BlockInterface
+{
+    /**
+    * @return {{class_name}}
+    */
+    protected function _beforeToHtml(): {{class_name}}
+    {
+        $this->setTemplate('');
+        return parent::_beforeToHtml();
+    }
+}
